96a9b2b25098f7612cc9d053cf1a84c5e4962dcf
barghest <bolshov03@mail.ru>
2021-11-25 08:20:15 +0300

    Bad drag implementation

--- src/View/View.ts
+++ src/View/View.ts
@@ -1,32 +1,34 @@
-import Thumb from "./ViewElements/Thumb/Thumb";
-import Track from "./ViewElements/Track/Track";
-import Observer from "../Observer/Observer";
-import Scale from "./ViewElements/Scale/Scale";
-import Fill from "./ViewElements/Fill/Fill";
+import Thumb from "./ViewElements/Thumb/Thumb"
+import Track from "./ViewElements/Track/Track"
+import Observer from "../Observer/Observer"
+import Scale from "./ViewElements/Scale/Scale"
+import Fill from "./ViewElements/Fill/Fill"
 class View extends Observer {
-	public thumbView: Thumb;
-	public trackView: Track;
-	public scaleView: Scale;
-	public fillView: Fill;
-	public parent: JQuery<HTMLElement>;
+	public thumbView: Thumb
+	public trackView: Track
+	public scaleView: Scale
+	public fillView: Fill
+	public parent: JQuery<HTMLElement>
 	constructor(sliderClass: string) {
-		super();
-		this.thumbView = new Thumb(this);
-		this.trackView = new Track(this);
-		this.scaleView = new Scale(this);
-		this.fillView = new Fill(this);
-		this.parent = $(sliderClass);
+		super()
+		this.thumbView = new Thumb(this)
+		this.trackView = new Track(this)
+		this.scaleView = new Scale(this)
+		this.fillView = new Fill(this)
+		this.parent = $(sliderClass)
 	}
 	public createViewSlider() {
-		this.trackView.createTrack();
-		this.thumbView.createThumb();
-		this.scaleView.createScale();
+
+		this.trackView.createTrack()
+		this.thumbView.createThumb()
+		this.scaleView.createScale()
 		this.fillView.createFill()
+
 	}
 
 	public updateViewSlider() {
-		this.thumbView.dragThumb();
+		this.thumbView.dragThumb()
 	}
 }
 
-export default View;
+export default View
--- src/View/ViewElements/Thumb/Thumb.ts
+++ src/View/ViewElements/Thumb/Thumb.ts
@@ -1,33 +1,45 @@
-import View from "../../View";
+import View from "../../View"
 
 class Thumb {
-	private parentElement: View;
-	private thumb: JQuery<HTMLElement>;
+	private parentElement: View
+	private thumb: JQuery<HTMLElement>
 	constructor(parentElement: View) {
-		this.parentElement = parentElement;
-		this.thumb = $(".slider__thumb");
+		this.parentElement = parentElement
+		this.thumb = $(".slider__thumb")
 	}
 	public createThumb() {
-
-			this.parentElement.parent.append('<div class="slider__thumb"></div>')
-			
+		this.parentElement.parent.append('<div class="slider__thumb"></div>')
 	}
 
 	public dragThumb() {
-			this.parentElement.parent.on('click',(e:JQuery.ClickEvent)=>{
-					console.log(e.offsetX);
-					
-					$('.slider__thumb').css({left: e.offsetX})  
-						$('.slider__fill').css({width:e.offsetX})	
-			})
+
+
 		this.parentElement.parent.on(
 			"mousedown",
-			".slider__thumb",
+			{ track: '.slider__track', thumb: '.slider__thumb' },
 			(event: JQuery.MouseDownEvent) => {
-						
+				$('body').on('mousemove', (e: JQuery.MouseMoveEvent) => {
+
+					const cursorPosition = {
+						x: e.pageX - $(event.currentTarget).position().left
+					}
+					
+					if (cursorPosition.x <= event.currentTarget.offsetWidth && cursorPosition.x >= 0) {
+						$(event.data.thumb).css({ left: cursorPosition.x })
+						$('.slider__fill').css({ width: cursorPosition.x })
+					}
+
+
+
+				})
+
 			}
-		);
+		)
+
+		$('body').on('mouseup', (event: JQuery.MouseUpEvent) => {
+			$('body').off('mousemove')
+		})
 	}
 }
 
-export default Thumb;
+export default Thumb
