7c54811b7d8066c2ae243c3762c357ec10ea3b02
barghest <bolshov03@mail.ru>
2021-11-28 22:43:55 +0000

    Added the ability to change the cursor position on click

--- src/View/ViewElements/Thumb/Thumb.ts
+++ src/View/ViewElements/Thumb/Thumb.ts
@@ -3,34 +3,30 @@ import View from "../../View"
 class Thumb {
 	private parentElement: View
 	private thumb: JQuery<HTMLElement>
+	private step: number
 	constructor(parentElement: View) {
 		this.parentElement = parentElement
 		this.thumb = $(".slider__thumb")
+			this.step = 12
 	}
 	public createThumb() {
 		this.parentElement.parent.append('<div class="slider__thumb"></div>')
 	}
 
 	public dragThumb() {
-
-
 		this.parentElement.parent.on(
 			"mousedown",
 			{ track: '.slider__track', thumb: '.slider__thumb' },
 			(event: JQuery.MouseDownEvent) => {
 				$('body').on('mousemove', (e: JQuery.MouseMoveEvent) => {
-
 					const cursorPosition = {
 						x: e.pageX - $(event.currentTarget).position().left
 					}
-					
-					if (cursorPosition.x <= event.currentTarget.offsetWidth && cursorPosition.x >= 0) {
+					if (cursorPosition.x <= event.currentTarget.offsetWidth  && cursorPosition.x >= 0) {
+							
 						$(event.data.thumb).css({ left: cursorPosition.x })
 						$('.slider__fill').css({ width: cursorPosition.x })
 					}
-
-
-
 				})
 
 			}
--- src/View/ViewElements/Track/Track.ts
+++ src/View/ViewElements/Track/Track.ts
@@ -1,16 +1,28 @@
-import View from "../../View"
+import View from "../../View";
 
 class Track {
-    private parentElement:View
-		public track:JQuery<HTMLElement>
-    constructor(parentElement:View) {
-        this.parentElement = parentElement
-				this.track = $(".slider__track")
-    }
-		public createTrack() {
-				this.parentElement.parent.append('<div class="slider__track"></div>')		
-		}
+	private parentElement: View;
+	public track: JQuery<HTMLElement>;
+	constructor(parentElement: View) {
+		this.parentElement = parentElement;
+		this.track = $(".slider__track");
+	}
+	public createTrack() {
+		this.parentElement.parent.append('<div class="slider__track"></div>');
+	}
+	public clickTrack() {
+		this.parentElement.parent.on(
+			"click",
+			{ track: ".slider__track" },
+			(e) => {
+				const cursorPosition = {
+					x: e.pageX - $(e.currentTarget).position().left,
+				};
+				$(".slider__thumb").css({ left: cursorPosition.x });
+				$(".slider__fill").css({ width: cursorPosition.x });
+			}
+		);
+	}
+}
 
-    }
-
-export default Track
+export default Track;
