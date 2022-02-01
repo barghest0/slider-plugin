d5e40a9bf04ed199ae6eb3ac5298505758eb18bc
barghest <bolshov03@mail.ru>
2022-02-01 15:56:19 +0000

    Update readme

--- README.md
+++ README.md
@@ -3,36 +3,39 @@
 [Пример реализации](https://barghest0.github.io/MetaLampSliderPlugin/dist/)
 
 ## Команды
+
 Команды могут осуществлятсья с помощью `npm` или `yarn` пакетных менеджеров:
 
-- Установка зависимостей - `npm i` или `yarn install`
+-   Установка зависимостей - `npm i` или `yarn install`
 
-- Запуск сервера для разработки - `npm run dev` или `yarn dev`
+-   Запуск сервера для разработки - `npm run dev` или `yarn dev`
 
-- Сборка проекта - `npm run build` или `yarn build`
+-   Сборка проекта - `npm run build` или `yarn build`
 
-- Запуск тестов jest - `npm run test` или `yarn test`
+-   Запуск тестов jest - `npm run test` или `yarn test`
 
-- Процентное покрытие тестами - `npm run test:coverage` или `yarn test:coverage`
+-   Процентное покрытие тестами - `npm run test:coverage` или `yarn test:coverage`
 
 ## Создание слайдера
+
 Инициализация:
 
+```javascript
+const slider = new Slider(".slider", {});
 ```
-const slider = new Slider('.slider',{})
-```
+
 Параметры:
 
-| Параметр        | Тип                     | Значение по умолчанию         | Описание                                                              |
-| --------------- | ------------------------|-------------------------------| ----------------------------------------------------------------------|
-| min             | number                  | 0                             | Минимальное значение                                                  |
-| max             | number                  | 100                           | Максимальное значение                                                 |
-| step            | number                  | 1                             | Шаг бегунка                                                           |
-| value           | number / number[]       | 0                             | Значение первого бегунок                                              |
-| isRange         | boolean                 | false                         | Создание 2 бегунка                                                    |
-| idDecimal       | boolean                 | false                         | Добавление к значению десятичной части                                |
-| decimalPlaces   | number                  | 0                             | Количество цифр после запятой                                         |
-| direction       | 'horizontal'/'vertical' | 'horizontal'                  | Направление слайдера (горизонтальное/вертикальное)                    |
-| hasScale        | boolean                 | true                          | Отображение шкалы                                                     |
-| hasTips         | boolean                 | true                          | Отображение подсказок над бегунками                                   |
-| hasFill         | boolean                 | true                          | Отображение прогресс бара                                             |
+| Параметр      | Тип                     | Значение по умолчанию | Описание                                           |
+| ------------- | ----------------------- | --------------------- | -------------------------------------------------- |
+| min           | number                  | 0                     | Минимальное значение                               |
+| max           | number                  | 100                   | Максимальное значение                              |
+| step          | number                  | 1                     | Шаг бегунка                                        |
+| value         | number / number[]       | 0                     | Значение первого бегунок                           |
+| isRange       | boolean                 | false                 | Создание 2 бегунка                                 |
+| idDecimal     | boolean                 | false                 | Добавление к значению десятичной части             |
+| decimalPlaces | number                  | 0                     | Количество цифр после запятой                      |
+| direction     | 'horizontal'/'vertical' | 'horizontal'          | Направление слайдера (горизонтальное/вертикальное) |
+| hasScale      | boolean                 | true                  | Отображение шкалы                                  |
+| hasTips       | boolean                 | true                  | Отображение подсказок над бегунками                |
+| hasFill       | boolean                 | true                  | Отображение прогресс бара                          |
--- tests/Demo/PreviewSlider.test.ts
+++ tests/Demo/PreviewSlider.test.ts
@@ -1,69 +1,67 @@
-import Panel from '../../src/Demo/Panel/Panel';
-import PreviewSlider from '../../src/Demo/PreviewSlider';
-import checkParams from '../../src/Presenter/PresenterModules/checkParams';
-import Slider from '../../src/Slider';
-import '@testing-library/jest-dom';
-import handleChange from '../../src/Demo/Panel/PanelModules/handleChange';
+import Panel from "../../src/Demo/Panel/Panel";
+import PreviewSlider from "../../src/Demo/PreviewSlider";
+import checkParams from "../../src/Presenter/PresenterModules/checkParams";
+import Slider from "../../src/Slider";
+import "@testing-library/jest-dom";
+import handleChange from "../../src/Demo/Panel/PanelModules/handleChange";
 
+describe("PreviewSlider test", () => {
+	document.body.innerHTML = '<div id="slider-1" class="slider-1"></div>';
+	const root = ".slider-1";
 
+	const previewSlider = new PreviewSlider(root, {});
+	const panel = new Panel(checkParams({}), root, previewSlider);
+	test("constructor test", () => {
+		expect(previewSlider.panel).toBeInstanceOf(Panel);
+		expect(previewSlider.slider).toBeInstanceOf(Slider);
+	});
 
-describe('PreviewSlider test', () => {
-  document.body.innerHTML = '<div id="slider-1" class="slider-1"></div>';
-  const root = '.slider-1';
+	test("init test", () => {
+		expect(previewSlider.panel.firstValueInput!.value).toBe("0");
+		expect(previewSlider.panel.firstValueInput).toBeInstanceOf(
+			HTMLInputElement
+		);
 
-  const previewSlider = new PreviewSlider(root, {});
-  const panel = new Panel(checkParams({}), root, previewSlider);
-  test('constructor test', () => {
-    expect(previewSlider.panel).toBeInstanceOf(Panel);
-    expect(previewSlider.slider).toBeInstanceOf(Slider);
-  });
+		panel.createPanel();
+		previewSlider.init(checkParams({ isRange: true }), "rebuild");
+		previewSlider.slider.params.isRange = true;
+	});
 
-  test('init test', () => {
-    expect(previewSlider.panel.firstValueInput!.value).toBe('0');
-    expect(previewSlider.panel.firstValueInput).toBeInstanceOf(HTMLInputElement);
+	test("correct update panel values", () => {
+		previewSlider.updatePanelValues(100, 0);
+		expect(previewSlider.panel.firstValueInput!.value).toBe("100");
+		previewSlider.updatePanelValues(200, 1);
+		expect(previewSlider.panel.secondValueInput!.value).toBe("200");
+	});
 
-    panel.createPanel();
-    previewSlider.init(checkParams({ isRange: true }), 'rebuild');
-    previewSlider.slider.params.isRange = true;
-  });
+	test("correct handle change", () => {
+		const event = new Event("input");
+		panel.createPanel();
 
-  test('correct update panel values', () => {
-    previewSlider.updatePanelValues(100, 0);
-    expect(previewSlider.panel.firstValueInput!.value).toBe('100');
-    previewSlider.updatePanelValues(200, 1);
-    expect(previewSlider.panel.secondValueInput!.value).toBe('200');
-  });
+		previewSlider.panel.firstValueInput!.dispatchEvent(event);
+		previewSlider.panel.firstValueInput!.value = "50";
 
-  test('correct handle change', () => {
-    const event = new Event('input');
-    panel.createPanel();
+		handleChange.call(panel, event, "value", 0);
+		expect(previewSlider.params.value[0]).toBe(50);
 
-    previewSlider.panel.firstValueInput!.dispatchEvent(event);
-    previewSlider.panel.firstValueInput!.value = '50';
+		previewSlider.panel.maxValueInput!.dispatchEvent(event);
+		previewSlider.panel.maxValueInput!.value = "200";
+		handleChange.call(panel, event, "max");
+		expect(previewSlider.params.max).toBe(200);
 
-    handleChange.call(panel, event, 'value', 0);
-    expect(previewSlider.params.value[0]).toBe(50);
+		previewSlider.panel.minValueInput!.dispatchEvent(event);
+		previewSlider.panel.minValueInput!.value = "200";
+		handleChange.call(panel, event, "min");
+		expect(previewSlider.params.min).toBe(190);
 
-    previewSlider.panel.maxValueInput!.dispatchEvent(event);
-    previewSlider.panel.maxValueInput!.value = '200';
-    handleChange.call(panel, event, 'max');
-    expect(previewSlider.params.max).toBe(200);
+		previewSlider.panel.isVertical!.dispatchEvent(event);
+		previewSlider.panel.isVertical!.checked = true;
+		handleChange.call(panel, event, "direction");
+		expect(previewSlider.params.direction).toBe("vertical");
 
-    previewSlider.panel.minValueInput!.dispatchEvent(event);
-    previewSlider.panel.minValueInput!.value = '200';
-    handleChange.call(panel, event, 'min');
-    expect(previewSlider.params.min).toBe(200);
-
-
-    previewSlider.panel.isVertical!.dispatchEvent(event);
-    previewSlider.panel.isVertical!.checked = true;
-    handleChange.call(panel, event, 'direction');
-    expect(previewSlider.params.direction).toBe('vertical');
-
-    previewSlider.panel.hasFill!.dispatchEvent(event);
-    previewSlider.panel.hasFill!.checked = true;
-    handleChange.call(panel, event, 'hasFill');
-    expect(previewSlider.params.hasFill).toBe(true);
-
-  });
+		previewSlider.panel.hasFill!.dispatchEvent(event);
+		previewSlider.panel.hasFill!.checked = true;
+		handleChange.call(panel, event, "hasFill");
+		expect(previewSlider.params.hasFill).toBe(true);
+	});
 });
