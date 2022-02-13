702b8ec77b4f8d5ff0bd4482157f237159e838a8
barghest <bolshov03@mail.ru>
2022-02-13 10:38:13 +0000

    Completed the validation of parameters with the same min and max values

--- src/Presenter/PresenterModules/validateParams/validateStep.ts
+++ src/Presenter/PresenterModules/validateParams/validateStep.ts
@@ -1,6 +1,6 @@
 function validateStep(step: number, min: number, max: number) {
 	let validatedStep = Math.min(Math.abs(min - max), step);
-	if (validatedStep < 0) validatedStep = 1;
+	if (validatedStep <= 0) validatedStep = step;
 
 	return validatedStep;
 }
