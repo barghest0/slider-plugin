484852860ce80d0b5af789f3835763412e76220a
barghest <bolshov03@mail.ru>
2022-04-05 15:33:45 +0000

    Update names in scale

--- src/View/SubViews/Scale/utils/prepareScaleData.ts
+++ src/View/SubViews/Scale/utils/prepareScaleData.ts
@@ -1,46 +1,53 @@
+import {
+  MAX_MULTIPLIER,
+  MIN_MULTIPLIER,
+  PRIMES,
+  SCALE_NUMBER_DECIMAL_PLACES,
+} from '../../../../constants/scale';
+import { MAX_PERCENTS } from '../../../../constants/slider';
 import { Direction, Directions } from '../../../../types/slider';
 
 function getDelimiter(dividend: number, delimiters: number[]): number {
-  for (let i = 0; i < delimiters.length; i += 1) {
-    if (dividend % delimiters[i] === 0) {
-      return delimiters[i];
+  for (const delimiter of delimiters) {
+    if (dividend % delimiter === 0) {
+      return delimiter;
     }
   }
   return getDelimiter(dividend - 1, delimiters);
 }
 
 function prepareScaleData(min: number, max: number, step: number, direction: Direction) {
-  const length = Math.round((max - min) / step + 1);
-  const lastIndex = length - 1;
+  const size = Math.round((max - min) / step + 1);
+  const lastIndex = size - 1;
 
-  const primes = [3, 5, 7, 11];
+  const delimiter = getDelimiter(lastIndex, PRIMES);
 
-  const delimiter = getDelimiter(lastIndex, primes);
-
-  let multiplier = Math.max(Math.floor(lastIndex / delimiter), 1);
-  multiplier = multiplier < 15 ? Math.min(multiplier, delimiter) : multiplier;
-
-  const linesCount = Math.ceil(length / multiplier);
+  let multiplier = Math.max(Math.floor(lastIndex / delimiter), MIN_MULTIPLIER);
+  const isLessThenMaxMultiplier = multiplier < MAX_MULTIPLIER;
+  multiplier = isLessThenMaxMultiplier ? Math.min(multiplier, delimiter) : multiplier;
 
   const range = Math.abs(max - min);
-  const isCountBigThanScalePoint = Math.floor(range / step) > linesCount;
-  const actualScaleSize = isCountBigThanScalePoint
-    ? Math.round(Math.round(range / step) / (linesCount - 1)) * step
+  const marksCount = Math.ceil(size / multiplier);
+  const lastMark = marksCount - 1;
+
+  const areGapsBiggerMarksCount = Math.floor(range / step) > marksCount;
+  const gapSize = areGapsBiggerMarksCount
+    ? Math.round(Math.round(range / step) / lastMark) * step
     : step;
 
-  const values = new Array(linesCount).fill(null).map((_, index) => {
-    let value = min + actualScaleSize * index;
+  const values = new Array(marksCount).fill(null).map((_, index) => {
+    let value = min + gapSize * index;
     value = Math.min(value, max);
-    if (index === linesCount - 1) value = max;
-    return Number(value.toFixed(1));
+    if (index === lastMark) value = max;
+    return Number(value.toFixed(SCALE_NUMBER_DECIMAL_PLACES));
   });
 
-  const offsets = new Array(linesCount).fill('').map((_, index) => {
-    let offset = Math.abs(actualScaleSize / range) * index * 100;
-    offset = Math.min(100, offset);
-    if (index === linesCount - 1) offset = 100;
-    if (direction === Directions.vertical) offset = Math.abs(100 - offset);
-    return Number(offset.toFixed(1));
+  const offsets = new Array(marksCount).fill(null).map((_, index) => {
+    let offset = Math.abs(gapSize / range) * index * MAX_PERCENTS;
+    offset = Math.min(MAX_PERCENTS, offset);
+    if (index === lastMark) offset = MAX_PERCENTS;
+    if (direction === Directions.vertical) offset = Math.abs(MAX_PERCENTS - offset);
+    return Number(offset.toFixed(SCALE_NUMBER_DECIMAL_PLACES));
   });
 
   const scaleData = {
