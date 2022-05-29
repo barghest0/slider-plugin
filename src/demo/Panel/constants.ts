b7a97d397c02abc5b5b6e08f662ef9561068e34d
barghest <bolshov03@mail.ru>
2022-05-29 15:47:33 +0000

    Change mix classes to data params for panel inputs

--- src/demo/Panel/constants.ts
+++ src/demo/Panel/constants.ts
@@ -1,19 +1,17 @@
 const PANEL_SELECTOR = '.js-slider-panel';
 const CHECKBOX_PANEL_SELECTOR = '.js-checkbox-panel';
 
-const MIN_SELECTOR = '.js-min-value';
-const MAX_SELECTOR = '.js-max-value';
-const FIRST_VALUE_SELECTOR = '.js-first-value';
-const SECOND_VALUE_SELECTOR = '.js-second-value';
-const STEP_SELECTOR = '.js-step';
-const DECIMAL_PLACES_SELECTOR = '.js-decimal-places';
-const IS_RANGE_SELECTOR = '.js-is-range';
-const IS_VERTICAL_SELECTOR = '.js-is-vertical';
-const HAS_SCALE_SELECTOR = '.js-has-scale';
-const HAS_FILL_SELECTOR = '.js-has-fill';
-const HAS_TIPS_SELECTOR = '.js-has-tips';
-const IS_DECIMAL_SELECTOR = '.js-is-decimal';
-const CAN_THUMB_PUSH_SELECTOR = '.js-can-thumb-push';
+const MIN_SELECTOR = '[data-param="min-value"]';
+const MAX_SELECTOR = '[data-param="max-value"]';
+const FIRST_VALUE_SELECTOR = '[ data-param="first-value" ]';
+const SECOND_VALUE_SELECTOR = '[data-param="second-value"]';
+const STEP_SELECTOR = '[data-param="step"]';
+const IS_RANGE_SELECTOR = '[data-param="is-range"]';
+const IS_VERTICAL_SELECTOR = '[data-param="is-vertical"]';
+const HAS_SCALE_SELECTOR = '[data-param="has-scale"]';
+const HAS_FILL_SELECTOR = '[data-param="has-fill"]';
+const HAS_TIPS_SELECTOR = '[data-param="has-tips"]';
+const CAN_THUMB_PUSH_SELECTOR = '[data-param="can-thumb-push"]';
 const CHECKBOX_TYPE = 'checkbox';
 const NUMBER_TYPE = 'number';
 
@@ -35,8 +33,6 @@ export {
   IS_VERTICAL_SELECTOR,
   IS_RANGE_SELECTOR,
   HAS_FILL_SELECTOR,
-  DECIMAL_PLACES_SELECTOR,
-  IS_DECIMAL_SELECTOR,
   HAS_TIPS_SELECTOR,
   CHECKBOX_PANEL_SELECTOR,
   CAN_THUMB_PUSH_SELECTOR,
